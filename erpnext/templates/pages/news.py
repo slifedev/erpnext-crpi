d855628aa28dcd8e98306792c56d59a004083756
elohero <duritsyn.v@gmail.com>
2024-10-17 14:31:23 +0000

    bugfix(templates/pages): button link, card photo, footer

--- erpnext/templates/pages/news.py
+++ erpnext/templates/pages/news.py
@@ -5,15 +5,31 @@ def get_context(context):
     # Отримати всі опубліковані блог-пости
     blog_posts = frappe.get_all('Blog Post', 
         filters={'published': 1}, 
-        fields=['title', 'route', 'published_on', 'blog_intro', 'meta_image', 'blog_category'],
+        fields=['title', 'route', 'published_on', 'blog_intro', 'meta_image', 'blog_category', 'blogger'],
         order_by="published_on desc"
     )
+
+
     for post in blog_posts:
         try:
             post['route'] =  post.get('route').split('/')[2]
         except Exception as e:
             post['route'] = ''
             print(str(e))
+
+        try:
+            bloggerData = frappe.db.get_value(
+                'Blogger',
+                post.blogger,
+                ['full_name', 'avatar'])
+            post['blogger_full_name'] = bloggerData[0] if bloggerData[0] else 'Unknown blogger'
+            post['blogger_photo'] = bloggerData[1] if bloggerData[1] else ''
+            print('post' , post)
+            print('bloggerData', bloggerData)
+        except Exception as e:
+            print("Error fetching blogger:", str(e))
+            post['blogger_full_name'] = 'Unknown Blogger'
+            post['blogger_photo'] = ''
 
     # Додати блог-пости в контекст для рендерингу в шаблоні
     context.blog_posts = blog_posts
